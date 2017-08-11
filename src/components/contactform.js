6f66d38287381b3bee0d04269a1d4d0502e4c9dd
Brigitta <baltwo@gmail.com>
2017-08-11 11:13:03 -0600

    add to contact form

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -7,7 +7,10 @@ export default class ContactForm extends React.Component {
       this.state = {
         name: '',
         email: '',
-        phone: ''
+        phone: '',
+        participant: false,
+        mentor: false,
+        instructor: false
       };
 
       this.handleInputChange = this.handleInputChange.bind(this);
@@ -51,6 +54,32 @@ export default class ContactForm extends React.Component {
           type="text"
           value={this.state.phone}
           onChange={this.handleInputChange} />
+        </label>
+
+        How would you like to be involved:
+        <label>
+          Participant:
+          <input
+            name="participant"
+            type="checkbox"
+            checked={this.state.participant}
+            onChange={this.handleInputChange} />
+        </label>
+        <label>
+          Mentor:
+          <input
+            name="mentor"
+            type="checkbox"
+            checked={this.state.mentor}
+            onChange={this.handleInputChange} />
+        </label>
+        <label>
+          Instructor:
+          <input
+            name="instructor"
+            type="checkbox"
+            checked={this.state.instructor}
+            onChange={this.handleInputChange} />
         </label>
         </form>
       );
