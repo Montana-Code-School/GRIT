9bbde5a4f4e5ecd21cc7bbaa7f9ebf55b60c33f9
merbaird <mnbaird@gmail.com>
2017-08-18 13:27:57 -0600

    optimized images for web and added a few to the page

--- client/src/components/mission.js
+++ client/src/components/mission.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Flexbox from 'flexbox-react';
-import gritAboutImg from '../img/gritAboutImg.jpg';
+import girlwithlumber from '../assets/img/GRIT_girl_with_lumber.jpg';
 
 export default class Mission extends React.Component {
   render () {
     return (
       <div>
         <h1> ABOUT GRIT </h1>
-        <img className="aboutImg" src={gritAboutImg} alt="woman mechanic working on a car" />
+        <img className="aboutImg" src={girlwithlumber} alt="girl working with table saw" />
         <p> Girls Representing in Trades, aims to empower middle and high school
         students who identify with womanhood by broadening their perspective of
         career options, strengthening their self-confidence, building their
