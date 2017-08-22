1d3e1e4d1c52ca2998c34ecd33778ccdb211898e
Brigitta <baltwo@gmail.com>
2017-08-22 17:02:21 -0600

    add some scrollmagic to main.js

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import gritHeaderImg from '../assets/img/gritHeaderImg.jpg';
+import ScrollMagic from 'scrollmagic';
+import Styles from '../assets/stylesheets/main.css';
+import {TimelineMax} from 'gsap';
+// require('scrollmagic/plugins/debug.addIndicators.js');
 
 export default class Main extends React.Component {
+
+  componentDidMount () {
+    this.controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onLeave'
+        }
+    });
+//get all sections
+    var sections = this.refs;
+
+        for (var key in sections) {
+            var note = ReactDOM.findDOMNode(sections[key]);
+	// create scene to pin and link animation
+            new ScrollMagic.Scene({
+                triggerElement: sections[key]
+            })
+              .setPin(sections[key])
+              .addIndicators() // add indicators (requires plugin)
+              .addTo(this.controller);
+        }
+    }
+
   render () {
     return (
-      <div>
-        <img className="headerImg" src={gritHeaderImg} alt="girl with welding mask" />
-        <h1>GRIT</h1>
-        <h2>GIRLS REPRESENTING IN TRADES</h2>
-        <a href="#"><button className="btn">Kendra&#39;s Story</button></a>
+      <div id="wrapper">
+      <div id="content">
+      <div className="scrollMagicPinSpacer">
+      <section className="introBlock" ref="introTrigger">
+        <section className="motionContainer" ref="introTrigger2">
+          <section className="motion left" id="motionLeft" ref="image">
+            <div className="contentContainer">
+              <h1>GRIT</h1>
+              <h2>GIRLS REPRESENTING IN TRADES</h2>
+            </div>
+            <div className="image" id="girlWithTableSaw">
+            </div>
+          </section>
+          <section className="motion right" id="motionRight" ref="story">
+            <a href="#"><button className="btn">Kendra&#39;s Story</button></a>
+          </section>
+        </section>
+        </section>
+      </div>
+      </div>
       </div>
     );
   }
