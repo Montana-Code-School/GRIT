33310c8c3bd1d1ac506c5e22c8a9ee7b067eda4f
April Trull <mtwoolybunnies@gmail.com>
2017-08-25 16:21:25 +0100

    nothing new

--- client/src/components/mission.js
+++ client/src/components/mission.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import girlwithlumber from '../assets/img/GRIT_girl_with_lumber.jpg';
 
+
+
 export default class Mission extends React.Component {
   render () {
     return (
-      <div>
-        <h1> ABOUT GRIT </h1>
+      <div className = "About" >
+        <h1 className ="title"> ABOUT GRIT </h1>
         <img className="aboutImg" src={girlwithlumber} alt="girl working with table saw" />
         <p> Girls Representing in Trades, aims to empower middle and high school
         students who identify with womanhood by broadening their perspective of
