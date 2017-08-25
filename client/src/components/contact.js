b90dbd0ede8494780aafce5d923f1435cddb80dc
merbaird <mnbaird@gmail.com>
2017-08-24 19:11:53 -0600

    started styling on main.js, mission.js

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -15,7 +15,7 @@ export default class Contact extends React.Component {
   render () {
     return (
       <div>
-        <h1> GET INVOLVED </h1>
+        <h1 className='orange'> GET INVOLVED </h1>
         <h3> 406.123.4567 </h3>
         <h4> ywcaofmissoula.org </h4>
         <img src={ConstructionPhoto} alt="group of female students at a contruction site"/>
--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import gritHeaderImg from '../assets/img/gritHeaderImg.jpg';
+import  '../assets/stylesheets/main.css';
 
 export default class Main extends React.Component {
   render () {
     return (
-      <div>
-        <img className="headerImg" src={gritHeaderImg} alt="girl with welding mask" />
-        <h1>GRIT</h1>
+      <div className='headerImg'>
+        <h1 className='grit'>GRIT</h1>
         <h2>GIRLS REPRESENTING IN TRADES</h2>
-        <a href="#"><button className="btn">Kendra&#39;s Story</button></a>
       </div>
     );
   }
