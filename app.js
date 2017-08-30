dfbb1fb224bb4b8b605dd2850c3ef0a79cf29af3
Jennifer Emerson <jenniferemerson@Jennifers-MacBook-Air.local>
2017-08-29 19:12:10 -0600

    added client back into dirname

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(logger('dev'));
 app.use(logger('combined', { stream: accessLogStream}));
 
 app.get('/', (req,res)=> {
-  res.sendFile(path.join(__dirname +'/src/index.js'));
+  res.sendFile(path.join(__dirname +'/client/src/index.js'));
   }
 );
 
