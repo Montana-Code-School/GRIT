122b315f49d4f22c3fbf6035b6fcc4625da8c09d
Brigitta <baltwo@gmail.com>
2017-08-31 19:22:15 -0600

    made styling fixes on stats component

--- client/src/components/statsShow.js
+++ client/src/components/statsShow.js
@@ -64,40 +64,47 @@ export default class statsShow extends React.Component {
 
             <div className="statsText fade">
               <div className="numbertext">1 / 6</div>
+               <div className="statsContent">
                 <h1> THE FUTURE IS FEMALE. AND TRADES. </h1>
-                  <p> "One solution to increasing labor participation is to get people in certain demographics - women, veterans and the disabled - to enter the workforce." Barb Wagner, the chief economist at the Montana Department of Labor and Industry. </p>
+                  <p> "One solution to increasing labor participation is to get people in certain demographics - women, veterans and the disabled - to enter the workforce." <br/></p><p id="quote"><em>– Barb Wagner, the chief economist at the Montana Department of Labor and Industry.</em></p>
+               </div>
               </div>
 
 
             <div className="statsText fade">
               <div className="numbertext">2 / 6</div>
-                <div>
+                <div className="statsContent">
                   <h1> WIDESPREAD TRADE SHORTAGES </h1>
-                    <p> Montana is facing an impending economic calamity if something isn&rsquo;t done to address an expected critical shortfall of workers. An estimated 16 million middle-skill jobs (jobs that require post-secondary education or certification that is not a bachelor&rsquo;s, or higher degree) will be created through 2024. </p>
+                    <p id="calamityText"> Montana is facing an impending economic calamity if something isn&rsquo;t done to address an expected critical shortfall of workers. An estimated 16 million middle-skill jobs (jobs that require post-secondary education or certification that is not a bachelor&rsquo;s, or higher degree) will be created through 2024. </p>
                   </div>
                 </div>
 
             <div className="statsText fade">
               <div className="numbertext">3 / 6</div>
+               <div className="statsContent">
                 <h1> THE CULPRITS </h1>
-                  <ul>
+                  <ul className="list">
                     <li> Widely-accepted stereotypes about women&rsquo;s physical inabilities. </li>
                     <li> A tolerance for sexual innuendo, homophobia and outright sexual harassment in the workplace. </li>
                     <li> Inadequate access to employment and training </li>
                   </ul>
+                </div>
               </div>
 
             <div className="statsText fade">
               <div className="numbertext">4 / 6</div>
+               <div className="statsContent">
                 <h1> WOMEN&rsquo;S WAGES </h1>
                   <p> The wage gap in the construction industry is suprisingly small. Women earn 91% of men&rsquo;s wages compared to the overall average of 82% across all professions. Jobs traditionally employed by men pay 20-30% more than jobs traditionally held by women. </p>
+               </div>
               </div>
 
               <div className="statsText fade">
                 <div className="numbertext">5 / 6</div>
+                  <div className="statsContent">
                   <h1> UNDERREPRESENTED </h1>
                     <p> In 2014, women made up 46.9% of workforce. Of that: </p>
-                      <ul>
+                      <ul className="list">
                         <li> 0.5% were roofers </li>
                         <li> 1.4% were automative service technicians and mechanics </li>
                         <li> 1.7% were carpenters </li>
@@ -105,13 +112,16 @@ export default class statsShow extends React.Component {
                         <li> 4.8% were welders </li>
                         <li> 5.7% were firefighters </li>
                       </ul>
+                    </div>
                   </div>
 
                 <div className="statsText fade">
                   <div className="numbertext">6 / 6</div>
+                   <div className="statsContent">
                     <h1> POOR ENROLLMENT </h1>
                       <p> Fewer than 5% of all students enrolled in high school technical
                       programs are girls. </p>
+                   </div>
                   </div>
 
             <a className="statsPrev" onClick={this.statsMinusSlidesClick}>&#10094;</a>
